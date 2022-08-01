61aa1c808c1c7106bdbd7b6345d24a7f5cde97a7
Seung Lee <shl7@uw.edu>
2022-08-01 14:50:26 -0700

    Update Department Index to show added departments
    Co-authored-by: Filmer Tolentino <filmer.tolentino@gmail.com>

--- UniversityRegistrar/Controllers/DepartmentsController.cs
+++ UniversityRegistrar/Controllers/DepartmentsController.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System;
 using Microsoft.AspNetCore.Mvc;
 using UniversityRegistrar.Models;
 using System.Linq;
@@ -21,6 +20,20 @@ namespace UniversityRegistrar.Controllers
       ViewBag.PageTitle = "View All Departments";
       List<Department> model = _db.Departments.ToList();
       return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.PageTitle = "Add a Department";
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Department department)
+    {
+      _db.Departments.Add(department);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
